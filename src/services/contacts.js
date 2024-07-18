eca155a5b9582b1e6279b3effc796295e54cacd7
Holovko Maksym <golovkomaksim852@gmail.com>
2024-07-18 22:10:39 +0300

    sort

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,7 +1,13 @@
 import { ContactsCollection } from '../../src/db/models/contacts.js';
+import { SORT_ORDER } from '../constants/index.js';
 import { calculatePaginationData } from '../utils/calculatePaginationData.js';
 
-export const getAllContacts = async ({ page, perPage }) => {
+export const getAllContacts = async ({
+  page = 1,
+  perPage = 10,
+  sortOrder = SORT_ORDER.ASC,
+  sortBy = '_id',
+}) => {
   const limit = perPage;
   const skip = (page - 1) * perPage;
 
@@ -10,12 +16,16 @@ export const getAllContacts = async ({ page, perPage }) => {
     .merge(contactsQuery)
     .countDocuments();
 
-  const students = await contactsQuery.skip(skip).limit(limit).exec();
+  const contacts = await contactsQuery
+    .skip(skip)
+    .limit(limit)
+    .sort({ [sortBy]: sortOrder })
+    .exec();
 
   const paginationData = calculatePaginationData(contactsCount, perPage, page);
 
   return {
-    data: students,
+    data: contacts,
     ...paginationData,
   };
 };
